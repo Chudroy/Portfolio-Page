4e4a8c25302755cad3dc5c676186492f83902ac8
chudroy <nijotigajo@gmail.com>
2022-08-15 12:17:27 +0200

    finesse darkmode

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component, HostBinding } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { DarkModeService } from './shared/services/dark-mode.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   lightClassName = 'my-light-theme';
   @HostBinding('class') className = '';
-  constructor(darkModeService: DarkModeService) {
-    darkModeService.darkMode$.subscribe((darkMode) => {
+  constructor(
+    private darkModeService: DarkModeService,
+    private overlay: OverlayContainer
+  ) {}
+
+  ngOnInit(): void {
+    this.darkModeService.darkMode$.subscribe((darkMode) => {
       this.className = darkMode ? '' : this.lightClassName;
+      if (darkMode) {
+        this.overlay
+          .getContainerElement()
+          .classList.remove(this.lightClassName);
+      } else if (!darkMode) {
+        this.overlay.getContainerElement().classList.add(this.lightClassName);
+      }
     });
   }
 }
